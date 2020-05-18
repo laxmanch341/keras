d6e8625438e9d1577c904469d8872170f708f4d3
root <laxmanch341@gmail.com>
2020-05-18 15:14:33 +0530

    hello

--- traindata.py
+++ traindata.py
@@ -23,6 +23,7 @@ model.add(Conv2D(256,(3,3),input_shape=X.shape[1:]))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Conv2D(128,(3,3))
+model.add(Conv2D(128,(3,3))
 
 
 
